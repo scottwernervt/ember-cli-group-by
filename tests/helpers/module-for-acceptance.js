4664cc3c234302d164c5aac8a07ddb4134773bf0
Scott Werner <scott.werner.vt@gmail.com>
2017-05-18 17:27:22 -0400

    Clean up eslint warnings and errors

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -17,6 +17,7 @@ export default function(name, options = {}) {
 
     afterEach() {
       let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+      // eslint-disable-next-line ember/named-functions-in-promises
       return Promise.resolve(afterEach).then(() => destroyApp(this.application));
     }
   });
