3220932d73bb55ee4f0200bbcd77f7b3bf2d8d4b
Scott Werner <scott.werner.vt@gmail.com>
2017-05-23 13:35:21 -0400

    Disable LTS tests (#2) and add  ember-2.10 as oldest version supported

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,35 +1,53 @@
 /* eslint-env node */
 module.exports = {
   scenarios: [
+    // LTS fail, see bug #2.
+    // {
+    //   name: 'ember-lts-2.4',
+    //   bower: {
+    //     dependencies: {
+    //       'ember': 'components/ember#lts-2-4'
+    //     },
+    //     resolutions: {
+    //       'ember': 'lts-2-4'
+    //     }
+    //   },
+    //   npm: {
+    //     devDependencies: {
+    //       'ember-source': null,
+    //       'ember-data': '2.9.0'
+    //     }
+    //   }
+    // },
+    // {
+    //   name: 'ember-lts-2.8',
+    //   bower: {
+    //     dependencies: {
+    //       'ember': 'components/ember#lts-2-8'
+    //     },
+    //     resolutions: {
+    //       'ember': 'lts-2-8'
+    //     }
+    //   },
+    //   npm: {
+    //     devDependencies: {
+    //       'ember-source': null,
+    //     }
+    //   }
+    // },
     {
-      name: 'ember-lts-2.4',
+      name: 'ember-2.10',
       bower: {
         dependencies: {
-          'ember': 'components/ember#lts-2-4'
+          'ember': 'components/ember#2.10.2'
         },
         resolutions: {
-          'ember': 'lts-2-4'
+          'ember': '2.10'
         }
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-8'
-        },
-        resolutions: {
-          'ember': 'lts-2-8'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
+          'ember-source': null,
         }
       }
     },
