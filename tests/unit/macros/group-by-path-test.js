10e83dce521090d4a50dc7be9c4d2e7fa8c04fc5
Scott Werner <scott.werner.vt@gmail.com>
2018-03-01 19:24:24 -0500

    Create new group by computed property

---
+++ tests/unit/macros/group-by-path-test.js
@@ -0,0 +1,17 @@
+import groupByPath from 'dummy/macros/group-by-path';
+import { module, test } from 'qunit';
+
+module('Unit | Macro | group by path');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  // compute({
+  //   assert,
+  //   computed: groupByPath('key1', 'key2'),
+  //   properties: {
+  //     key1: 1,
+  //     key2: 2
+  //   },
+  //   strictEqual: 3
+  // });
+});
