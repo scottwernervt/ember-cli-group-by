9cc6e3775f61a46d2ef8b99e069041f4583e957d
Scott Werner <scott.werner.vt@gmail.com>
2018-03-01 19:23:25 -0500

    Add groupBy helper for computed property and handlebars helper

---
+++ app/utils/group-by.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-group-by/utils/group-by';
---
+++ tests/unit/utils/group-by-test.js
@@ -0,0 +1,10 @@
+import groupBy from 'dummy/utils/group-by';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | group by');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = groupBy();
+  assert.ok(result);
+});
