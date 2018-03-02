7ca9a17d0e0bdf5ef53f5677c00cc3434528c99e
Scott Werner <scott.werner.vt@gmail.com>
2018-03-02 08:35:59 -0500

    Add unit tests for groupBy util function

--- tests/unit/utils/group-by-test.js
+++ tests/unit/utils/group-by-test.js
@@ -1,10 +1,18 @@
+import { A as emberA } from '@ember/array';
 import groupBy from 'dummy/utils/group-by';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | group by');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = groupBy();
-  assert.ok(result);
+test('It returns an empty object if array is empty', function (assert) {
+  let group = groupBy(emberA(), '');
+  assert.equal(typeof group, 'object');
+});
+
+
+test('It returns a promise proxy', function (assert) {
+  let group = groupBy(emberA(), '');
+  return group.then(function () {
+    assert.ok(group.get('isFulfilled'));
+  });
 });
