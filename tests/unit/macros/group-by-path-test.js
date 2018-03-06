9beb313c5665491ee1738d087d311db505bf03a0
Scott Werner <scott.werner.vt@gmail.com>
2018-03-05 19:14:48 -0500

    Add first test for computed property groupByPath

--- tests/unit/macros/group-by-path-test.js
+++ tests/unit/macros/group-by-path-test.js
@@ -1,17 +1,46 @@
-import groupByPath from 'dummy/macros/group-by-path';
-import { module, test } from 'qunit';
+import { A as emberA } from '@ember/array';
+import EmberObject from '@ember/object';
+import { groupByPath } from 'ember-cli-group-by/macros';
+import { module, skip, test } from 'ember-qunit';
 
-module('Unit | Macro | group by path');
+module('Unit | Macro | group by path', {
+  unit: true,
+});
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  // compute({
-  //   assert,
-  //   computed: groupByPath('key1', 'key2'),
-  //   properties: {
-  //     key1: 1,
-  //     key2: 2
-  //   },
-  //   strictEqual: 3
-  // });
+const TestSingle = EmberObject.extend({
+  array: null,
+  singleGroup: groupByPath('array', 'category'),
+});
+
+test('It groups by given single path', function (assert) {
+  const subject = TestSingle.create({
+    array: emberA([
+      { name: '1', category: 'A', },
+      { name: '2', category: 'A', },
+      { name: '3', category: 'B', },
+      { name: '4', category: 'B', },
+    ]),
+  });
+  return subject.get('singleGroup').then((grouped) => {
+    assert.deepEqual(Object.keys(grouped), ['A', 'B']);
+  });
+});
+
+const TestNested = EmberObject.extend({
+  array: null,
+  doubleGroup: groupByPath('array', 'category.type'),
+});
+
+skip('It groups by given nested path', function (assert) {
+  const subject = TestNested.create({
+    array: emberA([
+      { name: '1', category: { type: 'A' }, },
+      { name: '2', category: { type: 'A' }, },
+      { name: '3', category: { type: 'B' }, },
+      { name: '4', category: { type: 'B' }, },
+    ]),
+  });
+  return subject.get('doubleGroup').then((grouped) => {
+    assert.deepEqual(Object.keys(grouped), ['A', 'B']);
+  });
 });
