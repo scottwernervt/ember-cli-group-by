603598114246833b85d2e3641eb4c1f04faef26a
Scott Werner <scott.werner.vt@gmail.com>
2018-08-21 21:02:37 -0400

    Import setupTest from ember-qunit

--- tests/unit/utils/group-by-test.js
+++ tests/unit/utils/group-by-test.js
@@ -1,17 +1,18 @@
 import groupBy from 'dummy/utils/group-by';
-import { module, setupTest, test } from 'qunit';
+import { module, setupTest, test } from 'ember-qunit';
 
 module('Unit | Utility | group by', function (hooks) {
   setupTest(hooks);
 
+  test('It returns a promise proxy', function (assert) {
+    const group = groupBy([], '');
+    return group.then(function () {
+      assert.equal(group.get('isFulfilled'), true);
+    });
+  });
+
   test('It returns an empty object if array is empty', function (assert) {
     const group = groupBy([], '');
     assert.equal(typeof group, 'object');
-  });
-
-  test('It returns a promise proxy', async function (assert) {
-    const group = groupBy([], '');
-    const proxy = await group;
-    assert.ok(proxy.get('isFulfilled'));
   });
 });
